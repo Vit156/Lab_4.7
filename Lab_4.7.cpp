1f2b414cc7ca69624123a29c10e779ad93e59af1
unknown <malevych.vitaliy@gmail.com>
2025-10-08 02:17:42 +0300

    first

--- Lab_4.7.cpp
+++ Lab_4.7.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-    double xp, xk, x, dx, eps, a, R, S;
-    int n;
+    double xp, xk, x, dx, eps,  a = 0, R = 0, S = 0;
+    int n = 0;
 
     cout << "xp = "; cin >> xp;
     cout << "xk = "; cin >> xk;
@@ -15,28 +15,35 @@ int main()
 
     cout << fixed;
     cout << "-----------------------------------------------------" << endl;
-    cout << "|" << setw(5) << "x" << "     |"<< setw(12) << "exp(-x^2)" << "   |" << setw(8) << "S" << "       |" << setw(5) << "n" << "   |" << endl;
+    cout << "|" << setw(5) << "x" << "     |" << setw(12) << "exp^-x^2" << "   |" << setw(8) 
+         << "S" << "       |" << setw(5) << "n" << "   |" << endl;
     cout << "-----------------------------------------------------" << endl;
 
     x = xp;
-    while (x <= xk + 1e-10)   
+    while (x <= xk)
     {
         n = 0;
-        a = 1;              
+        a = 1;          
         S = a;
+
         do {
             n++;
-            R = (-1.0) * x * x / n;   
+            R = (-1) * x * x / n;   
             a *= R;
             S += a;
         } while (fabs(a) >= eps);
 
-        cout << "|" << setw(7) << setprecision(2) << x << "   |" << setw(12) << setprecision(5) << exp(-x * x) << "   |" << setw(12) << setprecision(5) << S << "   |" << setw(5) << n << "   |" << endl;
+        cout << "|" << setw(7) << setprecision(2) << x << "   |" << setw(12) << setprecision(6) << exp(-x * x) << "   |" 
+             << setw(12) << setprecision(6) << S << "   |" << setw(5) << n << "   |" << endl;
 
         x += dx;
     }
+
     cout << "-----------------------------------------------------" << endl;
 
     return 0;
 }
+
+
+
 
